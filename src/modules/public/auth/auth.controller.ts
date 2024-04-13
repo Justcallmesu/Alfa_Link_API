c9e645a07d57fbdfc31594a29694da2b6a935c1e
codenoobies <kevindubidaam@gmail.com>
2024-04-13 11:30:36 +0700

    add: Added Auth Route in auth.controller.ts

--- src/modules/public/auth/auth.controller.ts
+++ src/modules/public/auth/auth.controller.ts
@@ -1,5 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch } from '@nestjs/common';
 
+// Services
+import { AuthService } from './auth.service';
+@Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(private readonly AuthService: AuthService) {}
+
+  @Post('/login')
+  async Login() {}
+
+  @Post('/register')
+  async Register() {}
+
+  @Get('/logout')
+  async logout() {}
+
+  @Get('/me')
+  async getMe() {}
+
+  @Patch('/update-password')
+  async updatePassword() {}
+
+  @Post('/refresh-token')
+  async refreshToken() {}
 }
