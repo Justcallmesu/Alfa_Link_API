a851c5c84c53309323bdeda1e32c1fa67f0ddefe
codenoobies <kevindubidaam@gmail.com>
2024-04-14 14:03:02 +0700

    create: Created roles.seed.ts to seed Roles Data

--- src/seed.ts
+++ src/seed.ts
@@ -3,7 +3,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 // Module
-import { SeedModule } from './database/seeders/seed.module';
+import { SeedModule } from './database/seed.module';
 import { UserSeed } from './database/seeders/user.seed';
 
 // Env Package
