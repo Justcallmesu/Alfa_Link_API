953cd65e9347864d4b359c0d92d213235f7658da
codenoobies <kevindubidaam@gmail.com>
2024-04-15 08:51:24 +0700

    add: Added Request Context in JwtGuards

--- src/modules/common/guards/Jwt.Guard.ts
+++ src/modules/common/guards/Jwt.Guard.ts
@@ -1,9 +1,11 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
-    console.log(context);
+    const request: Request = context.switchToHttp().getRequest();
+
     return true;
   }
 }
