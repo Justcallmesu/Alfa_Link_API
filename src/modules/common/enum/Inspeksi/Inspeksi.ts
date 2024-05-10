d3b24693b388764741f170817f0f11101d7a8340
codenoobies <dev.justcallmesu@gmail.com>
2024-05-10 22:19:56 +0700

    add: Added Penjualan Status and Inspeksi Status

---
+++ src/modules/common/enum/Inspeksi/Inspeksi.ts
@@ -0,0 +1,8 @@
+export enum InspeksiStatus {
+  BARU = 'Mobil baru ',
+  CUCI = 'CUCI Mobil',
+  POLES = 'POLES Mobil',
+  BENGKEL = 'SERVIS Mobil',
+  INSPEKSI = 'Inspeksi Mobil',
+  SELESAI = 'Selesai',
+}
---
+++ src/modules/common/enum/penjualan/PenjualanEnum.ts
@@ -0,0 +1,34 @@
+export enum PenjualanStatus {
+  /**
+   * General
+   */
+  CHECKING = 'Customer Checking Mobil',
+  MOBIL_DISERAHKAN = 'Mobil Diserahkan',
+  SELESAI = 'Selesai',
+  PELUNASAN_DP = 'Pelunasan DP',
+
+  /**
+   * Status Kredit
+   */
+  PANJAR = 'Panjar',
+  SURVEY = 'Survey',
+  MENUNGGU_RESPON = 'Menunggu Respon Pusat',
+  PENAMBAHAN_DP = 'PENAMBAHAN DP',
+  MENERIMA_PENAMBAHAN_DP = 'Menerima Penambahan DP',
+
+  /**
+   * Status Cash
+   */
+  DITERIMA = 'Diterima',
+  NEGOSIASI = 'Negosiasi',
+
+  /**
+   * Berhasil
+   */
+  MENUNGGU_SURAT_PENCAIRAN = 'Menunggu Surat Pencairan',
+
+  /**
+   * GAGAL
+   */
+  TIDAK_SANGGUP_PENAMBAHAN_DP = 'Tidak Sanggup Penambahan DP',
+}
