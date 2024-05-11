6b07b01da5521bdf3c7ce30e5fb93770651aba70
codenoobies <dev.justcallmesu@gmail.com>
2024-05-11 17:21:54 +0700

    feat: Finished Fuel Type  CRUD

---
+++ src/modules/public/fuel_type/FuelType.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateFuelTypeDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
+  name: string;
+}
+
+export interface UpdateFuelTypeDto extends CreateFuelTypeDto {}
---
+++ src/modules/public/fuel_type/FuelType.service.ts
@@ -0,0 +1,99 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+// DTO
+import { CreateFuelTypeDto, UpdateFuelTypeDto } from './FuelType.dto';
+
+// Schema
+import {
+  FuelType,
+  FuelTypeDocument,
+} from '@/schemas/mobil/mobil_properties/FuelType';
+
+@Injectable()
+export class FuelTypeService {
+  constructor(
+    @InjectModel(FuelType.name) private fuelTypeModel: Model<FuelType>,
+  ) {}
+
+  async getAll(res: Response) {
+    const fuelTypeDatas = await this.fuelTypeModel.find({});
+
+    return res.json({
+      message: 'Data Fetched',
+      status: '200',
+      data: fuelTypeDatas,
+    });
+  }
+
+  async getOne(res: Response, id: string) {
+    const fuelTypeData: FuelTypeDocument | null =
+      await this.fuelTypeModel.findOne({
+        _id: id,
+      });
+
+    if (!fuelTypeData) {
+      throw new NotFoundException('Fuel Type Does Not Found');
+    }
+
+    res.json({
+      message: 'Data Fetched',
+      status: '200',
+      data: fuelTypeData,
+    });
+  }
+
+  async createFuelType(res: Response, body: CreateFuelTypeDto) {
+    const isExist = await this.fuelTypeModel.findOne({ name: body.name });
+
+    if (isExist) {
+      throw new ConflictException('Fuel Type Already Exist');
+    }
+
+    const fuelTypeData = await this.fuelTypeModel.create(body);
+
+    res.status(201).json({
+      message: 'Data Created',
+      status: '201',
+      data: fuelTypeData,
+    });
+  }
+  async updateMobil(res: Response, id: string, body: UpdateFuelTypeDto) {
+    const fuelTypeData: FuelTypeDocument | null =
+      await this.fuelTypeModel.findById(id);
+
+    if (!fuelTypeData) {
+      throw new NotFoundException('Fuel Type Doesnt Exist');
+    }
+
+    const updatedData = await fuelTypeData.updateOne(body);
+
+    res.status(200).json({
+      message: 'Data Updated',
+      status: '201',
+      data: updatedData,
+    });
+  }
+
+  async deleteMobil(res: Response, id: string) {
+    const fuelTypeData: FuelTypeDocument | null =
+      await this.fuelTypeModel.findById(id);
+
+    if (!fuelTypeData) {
+      throw new NotFoundException('Fuel Type Doesnt Exist');
+    }
+
+    await fuelTypeData.deleteOne();
+
+    res.status(204).json({
+      message: 'Data Deleted',
+      status: '204',
+    });
+  }
+}
