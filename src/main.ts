dc92b766b974e4e57934d85af0dbd252730a6525
codenoobies <dev.justcallmesu@gmail.com>
2024-06-29 14:39:20 +0700

    feat (main): created cors and added versioning into api

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,21 @@ config({ path: '.env' });
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'api/v',
+  });
 
   // Middleware
   app.useGlobalPipes(new ValidationPipe());
