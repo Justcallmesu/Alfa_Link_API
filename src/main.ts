1b183f3fa5c0d6ee8dd012e407088211a5e1a929
justcallmesu <kevindubidaam@gmail.com>
2024-07-31 10:01:30 +0700

    add (cors): added cors origin

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:4173'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:4173',
+      'http://localhost:5174',
+      'http://localhost:4174',
+    ],
     credentials: true,
   });
 
